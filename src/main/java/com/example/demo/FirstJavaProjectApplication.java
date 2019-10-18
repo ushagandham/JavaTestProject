9a99f58a3b703170adf023220e3becbfbcd1e8dc
Roop Padala <roop.padala@walmartlabs.com>
2019-10-17 20:02:06 -0700

    javafilescheckin

---
+++ src/main/java/com/example/demo/FirstJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaProjectApplication.class, args);
+	}
+
+}
